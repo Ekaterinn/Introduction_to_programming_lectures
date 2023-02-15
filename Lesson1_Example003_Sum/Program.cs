5970d26037ed44204a6a45a693469e108beaa4f8
Ekaterina <tkat1998@mail.ru>
2023-02-15 15:37:34 +0600

    save sum

---
+++ Lesson1_Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать программу сложени двух чисел
+Console.WriteLine("Введите первое и второе слагаемое. Выведена их сумма");
+int numberA = 6;
+int numberB = 8;
+// Второй вариант. Можно int result = (numberA + numberB);
+// Console.WriteLine(result); 
+Console.WriteLine(numberA + numberB);
