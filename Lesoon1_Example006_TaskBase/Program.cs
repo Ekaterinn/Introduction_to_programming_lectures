d2ac6dc96ef0a1d03ab49f190077e744726d4af0
Ekaterina <tkat1998@mail.ru>
2023-02-15 17:15:32 +0600

    save task

---
+++ Lesoon1_Example006_TaskBase/Program.cs
@@ -0,0 +1,14 @@
+﻿// Нужно определить самую большую гирю
+Console.WriteLine("Указан вес самой тяжелой гири");
+int numberA = 5;
+int numberB = 95;
+int numberC = 452;
+int numberD = 16;
+int numberE = 162;
+int max = numberA;
+if (max < numberA) max = numberA;
+if (max < numberB) max = numberB;
+if (max < numberC) max = numberC;
+if (max < numberD) max = numberD;
+if (max < numberE) max = numberE;
+Console.WriteLine(max);
