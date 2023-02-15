7a70cd9b7e775d9cdd77c411620cf0a2c7a74a3b
Ekaterina <tkat1998@mail.ru>
2023-02-15 16:54:55 +0600

    save if else

---
+++ Lesson1_Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// Приветствие пользователя-любимчика
+Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "катя")
+{
+    Console.WriteLine("Давно не виделись, Катя!");
+}
+else
+{
+    Console.WriteLine("Приветствую, ");
+    Console.WriteLine(username);
+}
