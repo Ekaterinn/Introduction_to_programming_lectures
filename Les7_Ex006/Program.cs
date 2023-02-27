546e9df6fb304f1699ef3fe8845e577c6d78f06b
Ekaterina <tkat1998@mail.ru>
2023-02-27 22:34:08 +0600

    save lesson2

---
+++ Les7_Ex006/Program.cs
@@ -0,0 +1,17 @@
+﻿// Как посмотреть содержимое папки?
+
+void CatalogInfo(string path, string indent = "")
+{
+ DirectoryInfo catalogs = new DirectoryInfo(path);
+ foreach (var currentCatalog in catalogs.GetDirectories())
+ {
+ Console.WriteLine($"{indent}{currentCatalog.Name}");
+ CatalogInfo(currentCatalog.FullName, indent + " ");
+ }
+ foreach (var item in catalogs.GetFiles())
+ {
+ Console.WriteLine($"{indent}{item.Name}");
+ }
+}
+string path = @"/D:\Acer_Document\IT\Знакомство_с_языками_программирования_лекции\Les2_Ex001_IntroMethod";
+CatalogInfo(path);
