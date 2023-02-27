a89edf1a79ee5ae9c282812b58cd5d51f4179adc
Ekaterina <tkat1998@mail.ru>
2023-02-27 21:59:34 +0600

    save lesson

---
+++ Les4_Ex003_Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿// Факториал 5! = 5*4*3*2*1
+
+// int Factorial(int N)
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if (N == 1) return 1;
+//     else return N* Factorial(N-1);
+// }
+// Console.WriteLine(Factorial(3)); // 1*2*3=6
+
+double Factorial(int N)
+{
+    // 1! = 1
+    // 0! = 1
+    if (N == 1) return 1;
+    else return N* Factorial(N-1);
+}
+for (int i = 1; i < 40; i++)
+{
+   Console.WriteLine($"{i}! = {Factorial(i)}");
+}
---
+++ Les4_Ex004_Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿// Фибоначчи - каждое слдующее число задается через предыдущее
+// 1 1 2 3 5 8 13 21...
+// F(1) = 1
+// F(2) = 1
+// F(n) = F(n-1) + F(n-2)
+
+// int Fibonacci(int N)
+// {
+//     if(N == 1 || N == 2) return 1;
+//     else return Fibonacci(N-1) + Fibonacci(N-2);
+// }
+// for (int i = 1; i < 10; i++)
+// {
+//     Console.WriteLine($"{i} = {Fibonacci(i)}");
+// }
+
+double Fibonacci(int N)
+{
+    if(N == 1 || N == 2) return 1;
+    else return Fibonacci(N-1) + Fibonacci(N-2);
+}
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"F({i}) = {Fibonacci(i)}");
+}
